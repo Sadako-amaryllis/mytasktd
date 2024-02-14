d8a8e97f3d5a4c55764151b34f44458753db0a9a
Mylene <mylene.mzali@edu.devinci.fr>
2024-02-14 13:52:18 +0100

    dashboard

--- app/src/main/java/com/example/td_android/MainActivity.kt
+++ app/src/main/java/com/example/td_android/MainActivity.kt
@@ -3,29 +3,38 @@ package com.example.td_android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.td_android.ui.AddTaskModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            val score = remember { mutableStateOf(0) }
 
             NavHost(navController, startDestination = "home") {
-                composable("home") { HomeScreen(navController) }
-                composable("quiz") { QuizScreen(navController, score) }
-                composable("result") { ResultScreen(navController, score.value) }
+                composable("home") {
+                    HomeScreen(navController)
+                }
+                composable("addTask") {
+                    AddTaskScreen(navController, AddTaskModel())
+                }
             }
         }
     }
@@ -33,16 +42,57 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Column {
-        Text(
-            text = "Welcome to the Quiz App!",
-            modifier = Modifier.padding(16.dp)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.vecteezy_ete_vacances_plage_icone_illustration_ou_3d_ete_icone_21658663),
+            contentDescription = null,
+            modifier = Modifier.size(300.dp),
+            contentScale = ContentScale.Fit
         )
-        Button(
-            onClick = { navController.navigate("quiz") },
-            modifier = Modifier.padding(16.dp)
+        Text(
+            text = "No task today!",
+            modifier = Modifier.padding(18.dp)
+        )
+        FloatingActionButton(
+            onClick =  { navController.navigate("addTask") },
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.End)
         ) {
-            Text("Start Quiz")
+            Text("+")
         }
     }
 }
+
+@Composable
+fun AddTaskScreen(
+    navController: NavController,
+    addTaskModel: AddTaskModel
+) {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        TaskForm(addTaskModel = addTaskModel)
+    }
+}
+
+@Composable
+fun TaskForm(
+    addTaskModel: AddTaskModel,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+
+        Button(
+            onClick = { addTaskModel.createTask() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text("Create task")
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/td_android/ui/AddTaskModel.kt
@@ -0,0 +1,67 @@
+package com.example.td_android.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+
+class AddTaskModel : ViewModel() {
+
+    private val _title = mutableStateOf("")
+    val title: String
+        get() = _title.value
+
+    private val _description = mutableStateOf("")
+    val description: String
+        get() = _description.value
+
+    private val _isImportant = mutableStateOf(false)
+    val isImportant: Boolean
+        get() = _isImportant.value
+
+    private val _deadline = mutableStateOf("")
+    val deadline: String
+        get() = _deadline.value
+
+    fun setTitle(title: String) {
+        _title.value = title
+    }
+
+    fun setDescription(description: String) {
+        _description.value = description
+    }
+
+    fun setIsImportant(isImportant: Boolean) {
+        _isImportant.value = isImportant
+    }
+
+    fun setDeadline(deadline: String) {
+        _deadline.value = deadline
+    }
+
+    fun createTask() {
+    }
+}
+
+@Composable
+fun TaskForm(
+    addTaskModel: AddTaskModel,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier.padding(16.dp)) {
+
+
+        Button(
+            onClick = { addTaskModel.createTask() },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Créer la tâche")
+        }
+    }
+}
